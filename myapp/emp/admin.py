229b48b955d534de0367390d298fc995e3b7a210
= <lathiyadhruvraj44@gmail.com>
2023-04-23 23:46:01 +0530

    admin panel updated

--- myapp/emp/admin.py
+++ myapp/emp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Emp
 
 # Register your models here.
+
+class EmpAdmin(admin.ModelAdmin):
+    list_display = ("name", "emp_id", "working")
+    list_editable = ("working",)
+    search_fields = ("name",)
+    list_filter = ("working",)
+
+admin.site.register(Emp, EmpAdmin)
